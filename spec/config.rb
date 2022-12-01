a6b2659f5fd451cd085d4cde2e015f69be713662
Carlos Hung <developertogo@gmail.com>
2022-12-01 13:05:30 -0800

    Update rspec db config

--- spec/config.rb
+++ spec/config.rb
@@ -3,7 +3,7 @@
 HTTP_SUCCESS = 200
 HTTP_UNPROCESSABLE = 422
 BASE_URL = 'http://localhost:4000'
-RESET_DB_COMMAND = 'mysql -u root -p yummy_development < yummy_development.sql.dump'
+RESET_DB_COMMAND = 'mysql -u root yummy < yummy.sql.dump'
 
 USER_ONE_ID = 1
 USER_TWO_ID = 2
